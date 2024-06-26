c8a52e9e911503618be326b521ff69432b22fa79
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-06-26 11:43:04 -0300

    first commit (tests in core)

---
+++ brute_force.sh
@@ -0,0 +1,35 @@
+echo STARTING...
+tce-load -wi compiletc git cmake openssl-dev zlib_base-dev ncursesw-dev libssh2-dev libcap-dev libgcrypt-dev autoconf automake perl5 m4 \
+&& \
+(
+git clone https://github.com/vanhauser-thc/thc-hydra.git
+cd /home/tc/thc-hydra
+./configure
+make
+sudo make install
+) & \
+(
+git clone https://github.com/openwall/john.git
+cd /home/tc/john/src
+./configure
+make
+alias john='/home/tc/john/run/john'
+) & \
+(
+git clone https://github.com/jmk-foofus/medusa.git
+cd /home/tc/medusa
+./configure
+make
+sudo make install
+) & \
+(
+git clone https://github.com/hashcat/hashcat.git
+cd /home/tc/hashcat
+make
+sudo make install
+) & \
+wait
+
+cd
+clear
+echo OK
